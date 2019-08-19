Challenge-Guy/woocommerce-payments

commit ac228bc1a1f7ffc54fb87d6bf8e4fb8a74cc5e2e

Use fresh data for transactions (#139)
* Add reducer with some basic actions

The reducer does define some actions, but never produces a different
state.

* Add function to initalize the data store for fresh-data

* Add function to initialize the fresh-data API client

* Add empty Payments API spec

This will be filled in as we go, but can remain empty for now since
we're not actually calling any Payments endpoints for data.

* Add dependencies to package.json

* Add function to initialize the Payments data store API

* Include the fresh-data Payments data store in web app

* Refactor TransactionsList to React Component class

This is necessary to use the component with fresh-data. Currently the
component always shows an empty table.

* Add withSelect function

This is used to wrap components before exporting them, and allows
components to access the fresh-data data store.

* Wrap TransactionsList export with `withSelect`

This will allow us to access the fresh-data data store.

* Add hard coded test data to transactions fresh-data selector

The selector really shouldn't be using hard coded data, but this is for
testing! All for the greater good, I'm sure.

* Add transactions selector to the Payments API spec

* Retrieve data to display in TransactionsList using transactions selector

By retrieving the relevant selectors we can access the data from the
fresh-data data store, and use that to populate the transactions table.

As far as I understand this (@coderkevin would be a better person to
ask) the `withSelect` method basically injects data from the selector
into the component's `this.props`, allowing us to use the data in the
component.

* Update package-lock.json

* Fix wrong parameter name in `createApiClient`

* Add reducer for `fresh-data`

* Add lodash to dependencies

* Add read operation for transactions list

This operation defines how requests are sent for `fresh-data`

client/index.js
@@ -10,6 +10,7 @@ import { addFilter } from '@wordpress/hooks';
 import './style.scss';
 import { HelloWorld } from 'hello-world';
 import TransactionsPage from 'transactions';
+import 'payments-api/payments-data-store';
 
 const DepositsPage = () => <HelloWorld>Hello from the deposits page</HelloWorld>;
 const DisputesPage = () => <HelloWorld>Hello from the disputes page</HelloWorld>;

client/payments-api/api-spec/payments-rest-api.js
@@ -1,0 +1,31 @@
+/** @format */
+
+/**
+ * Internal dependencies.
+ */
+import transactions from './transactions';
+
+function createPaymentsApiSpec() {
+	return {
+		name: 'wcPaymentsApi',
+		mutations: {},
+		selectors: {
+			...transactions.selectors,
+		},
+		operations: {
+			read( resourceNames ) {
+				return [
+					...transactions.operations.read( resourceNames ),
+				];
+			},
+			update( resourceNames, data ) {
+				return [];
+			},
+			updateLocally( resourceNames, data ) {
+				return [];
+			},
+		},
+	};
+}
+
+export default createPaymentsApiSpec();

client/payments-api/api-spec/transactions/index.js
@@ -1,0 +1,12 @@
+/** @format */
+
+/**
+ * Internal dependencies.
+ */
+import selectors from './selectors';
+import operations from './operations';
+
+export default {
+	selectors,
+	operations,
+};

client/payments-api/api-spec/transactions/operations.js
@@ -1,0 +1,44 @@
+/** @format */
+
+/**
+ * External dependencies.
+ */
+import apiFetch from '@wordpress/api-fetch';
+import { includes } from 'lodash';
+
+/**
+ * Internal dependencies.
+ */
+import { NAMESPACE } from '../../constants';
+
+function read( resourceNames, fetch = apiFetch, dataToResources = transactionsToResources ) {
+	return readTransactions( resourceNames, fetch, dataToResources );
+}
+
+export function readTransactions( resourceNames, fetch, dataToResources ) {
+	if ( includes( resourceNames, 'transactions-list' ) ) {
+		const url = `${ NAMESPACE }/payments/transactions`;
+
+		return [
+			fetch( { path: url } )
+				.then( dataToResources )
+				.catch( error => {
+					return { [ resourceName ]: { error } };
+				} )
+		];
+	}
+
+	return [];
+}
+
+export function transactionsToResources( transactions ) {
+	return {
+		[ 'transactions-list' ]: {
+			data: transactions,
+		}
+	};
+}
+
+export default {
+	read,
+};

client/payments-api/api-spec/transactions/selectors.js
@@ -1,0 +1,42 @@
+/** @format */
+
+/**
+ * External dependencies.
+ */
+import { isNil } from 'lodash';
+
+/**
+ * Internal dependencies.
+ */
+import { DEFAULT_REQUIREMENT } from '../../constants';
+
+const getTransactions = ( getResource, requireResource ) => (
+	requirement = DEFAULT_REQUIREMENT
+) => {
+	return requireResource( requirement, 'transactions-list' ).data || {};
+}
+
+const isWaitingForInitialLoad = ( getResource ) => () => {
+	const resourceName = 'transactions-list';
+	const transactionsResource = getResource( resourceName );
+
+	return transactionsResource.lastReceived === undefined;
+}
+
+const getTransactionsIsLoading = ( getResource ) => () => {
+	const resourceName = 'transactions-list';
+	const transactionsResource = getResource( resourceName );
+
+	return transactionsResource.lastRequested > transactionsResource.lastReceived;
+}
+
+const showTransactionsPlaceholder = ( getResource ) => () => {
+	return isWaitingForInitialLoad( getResource )();
+}
+
+export default {
+	getTransactions,
+	getTransactionsIsLoading,
+	isWaitingForInitialLoad,
+	showTransactionsPlaceholder,
+};

client/payments-api/constants.js
@@ -1,0 +1,12 @@
+
+/**
+ * External dependencies.
+ */
+import { MINUTE } from '@fresh-data/framework';
+
+export const DEFAULT_REQUIREMENT = {
+	timeout: 1 * MINUTE,
+	freshness: 30 * MINUTE,
+};
+
+export const NAMESPACE = '/wc/v3';

client/payments-api/payments-data-store/create-api-client.js
@@ -1,0 +1,45 @@
+
+/**
+ * External dependencies.
+ */
+import { ApiClient } from '@fresh-data/framework';
+
+/**
+ * Internal dependencies.
+ */
+import createStore from './create-store';
+
+function createDataHandlers( store ) {
+	return {
+		dataRequested: resourceNames => {
+			store.dispatch( {
+				type: 'FRESH_DATA_REQUESTED',
+				resourceNames,
+				time: new Date(),
+			} );
+		},
+		dataReceived: resources => {
+			store.dispatch( {
+				type: 'FRESH_DATA_RECEIVED',
+				resources,
+				time: new Date(),
+			} );
+		},
+	};
+}
+
+function createApiClient( name, apiSpec ) {
+	const store = createStore( name );
+	const dataHandlers = createDataHandlers( store );
+	const apiClient = new ApiClient( apiSpec );
+	apiClient.setDataHandlers( dataHandlers );
+
+	const storeChanged = () => {
+		apiClient.setState( store.getState() );
+	}
+	store.subscribe( storeChanged );
+
+	return apiClient;
+}
+
+export default createApiClient;

client/payments-api/payments-data-store/create-store.js
@@ -1,0 +1,16 @@
+
+/**
+ * External dependencies.
+ */
+import { createStore } from 'redux';
+
+/**
+ * Internal dependencies.
+ */
+import reducer from './reducer';
+
+export default name => {
+	const devTools = window.__REDUX_DEVTOOLS_EXTENSION__;
+
+	return createStore( reducer, devTools && devTools( { name: name, instanceId: name } ) );
+}

commit 5dd7f9740974e730ab68b467d19aeb1469d26fe5

Fix linting issues (#180)
* Fix linting issues introduced in #139

* Fix linting issues in hello-world

* Fix linting issues in payments client

* Suppress empty source warning for scss files

client/hello-world/index.js
@@ -13,7 +13,7 @@ class HelloWorld extends Component {
 		return (
 			<div className="woocommerce-payments__section">
 				{ this.props.children || 'Hello, world.' }
-    		</div>
+			</div>
 		);
 	}
 }

client/index.js
@@ -1,3 +1,5 @@
+/** @format **/
+
 /**
  * External dependencies
  */

client/payments-api/api-spec/payments-rest-api.js
@@ -1,7 +1,7 @@
 /** @format */
 
 /**
- * Internal dependencies.
+ * Internal dependencies
  */
 import transactions from './transactions';
 
@@ -18,10 +18,14 @@ function createPaymentsApiSpec() {
 					...transactions.operations.read( resourceNames ),
 				];
 			},
+			/* eslint-disable */
 			update( resourceNames, data ) {
+				/* eslint-enable */
 				return [];
 			},
+			/* eslint-disable */
 			updateLocally( resourceNames, data ) {
+				/* eslint-enable */
 				return [];
 			},
 		},

client/payments-api/api-spec/transactions/index.js
@@ -1,7 +1,7 @@
 /** @format */
 
 /**
- * Internal dependencies.
+ * Internal dependencies
  */
 import selectors from './selectors';
 import operations from './operations';

client/payments-api/api-spec/transactions/operations.js
@@ -1,13 +1,13 @@
 /** @format */
 
 /**
- * External dependencies.
+ * External dependencies
  */
 import apiFetch from '@wordpress/api-fetch';
 import { includes } from 'lodash';
 
 /**
- * Internal dependencies.
+ * Internal dependencies
  */
 import { NAMESPACE } from '../../constants';
 
@@ -23,8 +23,8 @@ export function readTransactions( resourceNames, fetch, dataToResources ) {
 			fetch( { path: url } )
 				.then( dataToResources )
 				.catch( error => {
-					return { [ resourceName ]: { error } };
-				} )
+					return { [ 'transactions-list' ]: { error } };
+				} ),
 		];
 	}
 
@@ -35,7 +35,7 @@ export function transactionsToResources( transactions ) {
 	return {
 		[ 'transactions-list' ]: {
 			data: transactions,
-		}
+		},
 	};
 }
 

client/payments-api/api-spec/transactions/selectors.js
@@ -1,12 +1,7 @@
 /** @format */
 
 /**
- * External dependencies.
- */
-import { isNil } from 'lodash';
-
-/**
- * Internal dependencies.
+ * Internal dependencies
  */
 import { DEFAULT_REQUIREMENT } from '../../constants';
 
@@ -14,25 +9,25 @@ const getTransactions = ( getResource, requireResource ) => (
 	requirement = DEFAULT_REQUIREMENT
 ) => {
 	return requireResource( requirement, 'transactions-list' ).data || {};
-}
+};
 
 const isWaitingForInitialLoad = ( getResource ) => () => {
 	const resourceName = 'transactions-list';
 	const transactionsResource = getResource( resourceName );
 
 	return transactionsResource.lastReceived === undefined;
-}
+};
 
 const getTransactionsIsLoading = ( getResource ) => () => {
 	const resourceName = 'transactions-list';
 	const transactionsResource = getResource( resourceName );
 
 	return transactionsResource.lastRequested > transactionsResource.lastReceived;
-}
+};
 
 const showTransactionsPlaceholder = ( getResource ) => () => {
 	return isWaitingForInitialLoad( getResource )();
-}
+};
 
 export default {
 	getTransactions,

client/payments-api/constants.js
@@ -1,6 +1,7 @@
+/** @format **/
 
 /**
- * External dependencies.
+ * External dependencies
  */
 import { MINUTE } from '@fresh-data/framework';
 

client/payments-api/payments-data-store/create-api-client.js
@@ -1,11 +1,12 @@
+/** @format **/
 
 /**
- * External dependencies.
+ * External dependencies
  */
 import { ApiClient } from '@fresh-data/framework';
 
 /**
- * Internal dependencies.
+ * Internal dependencies
  */
 import createStore from './create-store';
 
@@ -36,7 +37,7 @@ function createApiClient( name, apiSpec ) {
 
 	const storeChanged = () => {
 		apiClient.setState( store.getState() );
-	}
+	};
 	store.subscribe( storeChanged );
 
 	return apiClient;

client/payments-api/payments-data-store/create-store.js
@@ -1,11 +1,12 @@
+/** @format **/
 
 /**
- * External dependencies.
+ * External dependencies
  */
 import { createStore } from 'redux';
 
 /**
- * Internal dependencies.
+ * Internal dependencies
  */
 import reducer from './reducer';
 
@@ -13,4 +14,4 @@ export default name => {
 	const devTools = window.__REDUX_DEVTOOLS_EXTENSION__;
 
 	return createStore( reducer, devTools && devTools( { name: name, instanceId: name } ) );
-}
+};

client/payments-api/payments-data-store/index.js
@@ -1,11 +1,12 @@
+/** @format **/
 
 /**
- * External dependencies.
+ * External dependencies
  */
 import { registerGenericStore } from '@wordpress/data';
 
 /**
- * Internal dependencies.
+ * Internal dependencies
  */
 import createApiClient from './create-api-client';
 import paymentsApiSpec from '../api-spec/payments-rest-api';

client/payments-api/payments-data-store/reducer.js
@@ -1,3 +1,4 @@
+/** @format **/
 
 const defaultState = {
 	resources: {},

client/payments-api/with-select.js
@@ -189,7 +189,7 @@ const withSelect = mapSelectToProps =>
 			<RegistryConsumer>
 				{ registry => <ComponentWithSelect ownProps={ ownProps } registry={ registry } /> }
 			</RegistryConsumer>
-		)
+		);
 	}, 'withSelect' );
 
 export default withSelect;

client/style.scss
@@ -1,0 +1,2 @@
+/* stylelint-disable */
+/* stylelint-enable */

client/transactions/index.js
@@ -1,3 +1,5 @@
+/** @format **/
+
 /**
  * External dependencies
  */
@@ -41,7 +43,7 @@ export const TransactionsList = ( props ) => {
 		const outcome = charge ? charge.outcome : null;
 		const payment_method_details = charge ? charge.payment_method_details : null;
 		const address = billing_details ? billing_details.address : null;
-		const card = payment_method_details ?  payment_method_details.card : null;
+		const card = payment_method_details ? payment_method_details.card : null;
 
 		// Map transaction into table row.
 		const data = {

client/hello-world/test/index.js
@@ -11,7 +11,7 @@ import { HelloWorld } from '../';
 
 describe( 'HelloWorld', () => {
 	test( 'it renders correctly', () => {
-		const tree = renderer.create( <HelloWorld/> ).toJSON();
+		const tree = renderer.create( <HelloWorld /> ).toJSON();
 		expect( tree ).toMatchSnapshot();
 	} );
 } );

client/payments-api/test/api-spec/transactions/index.js
@@ -1,3 +1,7 @@
 /** @format */
+
+/**
+ * Internal dependencies
+ */
 import './operations';
 import './selectors';

client/payments-api/test/api-spec/transactions/operations.js
@@ -1,7 +1,7 @@
 /** @format */
 
 /**
- * Internal dependencies.
+ * Internal dependencies
  */
 import { readTransactions, transactionsToResources } from '../../../api-spec/transactions/operations';
 import { NAMESPACE } from '../../../constants';
@@ -13,7 +13,7 @@ describe( 'Transactions operations', () => {
 		it( 'Returns a list with one promise when correct resource names are supplied', () => {
 			const mockData = [ {}, {}, {} ];
 			const expectedResolvedPromise = {
-				[ "transactions-list" ]: {
+				[ 'transactions-list' ]: {
 					data: mockData,
 				},
 			};
@@ -33,7 +33,7 @@ describe( 'Transactions operations', () => {
 			expect( mockFetch ).toHaveBeenCalledTimes( 1 );
 			expect( mockFetch ).toHaveBeenCalledWith( { path: expectedUrl } );
 			expect( promises ).toStrictEqual( expectedPromises );
-			promises[0].then( result => {
+			promises[ 0 ].then( result => {
 				expect( mockToResources ).toHaveBeenCalledTimes( 1 );
 				expect( mockToResources ).toHaveBeenCalledWith( mockData );
 				expect( result ).toBe( expectedResolvedPromise );

client/payments-api/test/api-spec/transactions/selectors.js
@@ -1,11 +1,10 @@
 /** @format */
 
 /**
- * Internal dependencies.
+ * Internal dependencies
  */
 import transactionsSelectors from '../../../api-spec/transactions/selectors';
 import { DEFAULT_REQUIREMENT } from '../../../constants';
-
 
 describe( 'Transactions selectors', () => {
 	const expectedResourceName = 'transactions-list';
@@ -108,7 +107,7 @@ describe( 'Transactions selectors', () => {
 			expect( initStatus ).toBe( expected );
 		} );
 
-		it( "Returns true when first request in flight", () => {
+		it( 'Returns true when first request in flight', () => {
 			const expected = true;
 
 			const mockGetResource = jest.fn();
@@ -138,7 +137,7 @@ describe( 'Transactions selectors', () => {
 			expect( showPlaceholder ).toBe( expected );
 		} );
 
-		it( "Returns true when first request in flight", () => {
+		it( 'Returns true when first request in flight', () => {
 			const expected = true;
 
 			const mockGetResource = jest.fn();
@@ -153,7 +152,7 @@ describe( 'Transactions selectors', () => {
 			expect( initStatus ).toBe( expected );
 		} );
 
-		it( "Returns false when transactions are initialized", () => {
+		it( 'Returns false when transactions are initialized', () => {
 			const expected = false;
 
 			const mockGetResource = jest.fn();

client/payments-api/test/index.js
@@ -1,3 +1,6 @@
 /** @format */
 
+/**
+ * Internal dependencies
+ */
 import './api-spec/transactions';